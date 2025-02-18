6189fc510bcfe00146b59985ee28ee664a4ce726
Piioni <rangelc.juane@gmail.com>
2025-02-18 21:00:44 +0100

    New configuration scene to let user manage the name, direcction and description to the stores

---
+++ src/Classes/Configurator.java
@@ -0,0 +1,25 @@
+package Classes;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Configurator {
+    private TextField txtNombre, txtDireccion, txtDescripcion;
+    private Button btnGuardar, btnLimpiar;
+    private Label lblNombre, lblDireccion, lblDescripcion;
+    private Stage stage;
+
+
+    public Configurator(Stage stage, Tienda tienda) {
+        this.stage = stage;
+    }
+
+    public Scene getScene() {
+
+
+
+        return null;
+    }
+}
